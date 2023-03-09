7b0a5950ebac82a75c44994d9157095bc515f9fc
priya-km <priya.k.mah@gmail.com>
2023-03-09 15:09:23 -0500

    Added Genre and Director models in models.js, updated index.js with code up to date with mongoose version for all end points.

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const Models = require('./models.js'),
 // Importing the models
 const Movies = Models.Movie;
 const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
 // Connecting Mongoose with Rest API ad myflixDB
-mongoose.connect('mongodb://localhost:27017/myflixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/myflixDB').
+  catch(error => handleError(error));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,6 +36,7 @@ app.post('/users', (req, res) => {
       } else {
         Users.create({ /* if user doesn't exist then Users.create mongoose' CREATE command is used to create the new user */
           Username: req.body.Username,
+          Name: req.body.Name,
           Password: req.body.Password,
           Email: req.body.Email,
           Birthday: req.body.Birthday
@@ -76,72 +80,64 @@ app.get('/users/:Username', (req, res) => {
 
 // CREATE - Allow users to add a movie to their list of favorites
 app.post('/users/:Username/movies/:MovieID', (req, res) => {
-  Users.findOneAndUpdate({ Username: req.params.Username }, {
+  const promise = Users.findOneAndUpdate({ Username: req.params.Username }, {
     $push: { FavoriteMovies: req.params.MovieID } /* using $push to add a new movieID onto the end of the favorite movies array. */
   },
-    { new: true }, /* makes sure updated data is returned */
-    (err, updatedUser) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      } else {
-        res.json(updatedUser);
-      }
+    { new: true }) /* makes sure updated data is returned */
+    .exec();
+
+  promise.then((updatedUser) => {
+      res.json(updatedUser);
     });
 });
 
+
+
 // UPDATE User info
 app.put('/users/:Username', (req, res) => {
-  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
-    {
+  const promise = Users.findOneAndUpdate({ Username: req.params.Username }, { $set: {
       Username: req.body.Username,
+      Name: req.body.Name,
       Password: req.body.Password,
       Email: req.body.Email,
       Birthday: req.body.Birthday
     }
   },
-  { new: true }, // This line makes sure that the updated document is returned
-  (err, updatedUser) => {
-    if(err) {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    } else {
-      res.json(updatedUser);
-    }
+  { new: true }) // This line makes sure that the updated document is returned
+  .exec();
+
+  promise.then((updatedUser) => {
+    res.json(updatedUser);
   });
 });
 
 // DELETE - Allow users to remove a movie from their list of favorites
 app.delete('/users/:Username/movies/:MovieID', (req, res) => {
-  Users.findOneAndUpdate({ Username: req.params.Username }, {
-    $pull: { FavoriteMovies: req.params.MovieID } /* using $push to add a new movieID onto the end of the favorite movies array. */
+  const promise = Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $pull: { FavoriteMovies: req.params.MovieID } /* using $pull to remove movieID from favorite movies array. */
   },
-    { new: true }, /* makes sure updated data is returned */
-    (err, updatedUser) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      } else {
-        res.json(updatedUser);
-      }
+    { new: true }) /* makes sure updated data is returned */
+    .exec();
+
+  promise.then((updatedUser) => {
+      res.json(updatedUser);
     });
 });
 
+
 // Delete a user by username
 app.delete('/users/:Username', (req, res) => {
-  Users.findOneAndRemove({ Username: req.params.Username })
-    .then((user) => { /* checking if the document exists, if it does it gets deleted, if not it responds with was not found */
+  const promise = Users.findOneAndRemove({ Username: req.params.Username }).exec();
+
+  promise.then((user) => { /* checking if the document exists, if it does it gets deleted, if not it responds with was not found */
       if (!user) {
         res.status(400).send(req.params.Username + ' was not found');
       } else {
         res.status(200).send(req.params.Username + ' was deleted.');
       }
-    })
-    .catch((err) => { /* error handling */
-      console.error(err);
-      res.status(500).send('Error: ' + err);
     });
 });
+
 
 // READ - Get list of all movies
 app.get('/movies', (req, res) => {
@@ -157,7 +153,7 @@ app.get('/movies', (req, res) => {
 // Find movie by title
 app.get('/movies/:Title', (req, res) => {
   Movies.findOne({ Title: req.params.Title })
-    .then((movies) => { /* sending the response with user data back to the client */
+    .then((movies) => { 
       res.json(movies);
     })
     .catch((err) => { /* error handling */
@@ -168,10 +164,10 @@ app.get('/movies/:Title', (req, res) => {
 
 
 // Find movie by genre
-app.get('/movies/genre/:genreName', (req, res) => {
-  Movies.findOne({ "Genre.Name": req.params.genreName })
-    .then((movies) => { /* sending the response with user data back to the client */
-      res.json(movie.genre);
+app.get('/movies/Genre/:genreName', (req, res) => {
+  Movies.findOne({ 'Genre.Name': req.params.genreName })
+    .then((movies) => { 
+      res.json(movies.Genre);
     })
     .catch((err) => { /* error handling */
       console.error(err);
@@ -180,10 +176,10 @@ app.get('/movies/genre/:genreName', (req, res) => {
 });
 
 // Find data about director by name
-app.get('/movies/directors/:directorName', (req, res) => {
-  Movies.findOne({ "Director.Name": req.params.directorName })
-    .then((movies) => { /* sending the response with user data back to the client */
-      res.json(movie.director);
+app.get('/movies/Director/:directorName', (req, res) => {
+  Movies.findOne({ 'Director.Name': req.params.directorName })
+    .then((movies) => {
+      res.json(movies.Director);
     })
     .catch((err) => { /* error handling */
       console.error(err);
