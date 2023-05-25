bc817d85c72e46ec9ab72b8fe4154ab89848d817
priya-km <priya.k.mah@gmail.com>
2023-05-25 18:20:34 -0400

    added allowedOrigins to index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const Genres = Models.Genre;
 const Directors = Models.Director;
 
 const cors = require("cors");
+let allowedOrigins = ["http://localhost:3000", "http://localhost:1234"];
 app.use(cors());
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
